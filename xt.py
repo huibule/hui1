ed24284f6b7acc08ecb267d756d6fcc619525d9b
huibule <3338299673@qq.com>
2025-04-17 11:25:11 +0800

    Add files via upload

--- xt.py
+++ xt.py
@@ -1,19 +1,20 @@
 from langchain_openai import ChatOpenAI
-
-model = ChatOpenAI(model="moonshot-v1-8k",
-                  openai_api_key=api_key,
-                  openai_api_base="https://api.moonshot.cn/v1")
 from langchain.prompts import ChatPromptTemplate
-
-prompt = ChatPromptTemplate.from_messages([
-            ("system", system_template_text),
-            ("human", human_template_text)
-            ])
 from langchain.output_parsers import PydanticOutputParser
+from 小红书2_template import xiaohongshu_template,user_template
+from 小红书1_model import Xiaohongshu
+def ok(subject,api_key):
+    model = ChatOpenAI(model="moonshot-v1-8k",
+                       openai_api_key=api_key,
+                       openai_api_base="https://api.moonshot.cn/v1")
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", xiaohongshu_template),
+        ("human", user_template)
+    ])
+    output_parser = PydanticOutputParser(pydantic_object=Xiaohongshu)
+    parser_instructions = output_parser.get_format_instructions()
+    result = (prompt | model | output_parser).invoke(
+        {"subject": subject,
+         "parser_instructions": parser_instructions})
+    return result
 
-output_parser = PydanticOutputParser()
-parser_instructions = output_parser.get_format_instructions()
-
-result = (prompt | model | output_parser).invoke(
-                   {"input_language": input_language,
-                    "parser_instructions": parser_instructions})
